5e1e4c7ee66b156f3614cb9240ea8c40b4eae56c
allen.joseph.v.a@gmail.com <allen.joseph.v.a@gmail.com@7f3ace17-440a-c7af-bd5e-36327ecdc612>
2013-12-18 07:01:24 +0000

    avanse

--- PlatAcad/src/main/java/com/platacad/controllers/AporteController.java
+++ PlatAcad/src/main/java/com/platacad/controllers/AporteController.java
@@ -1,9 +1,14 @@
 package com.platacad.controllers;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.platacad.model.entities.Articulo;
@@ -29,5 +34,20 @@ public class AporteController {
     	ModelAndView model = new ModelAndView("aporte");
     	Integer estado = articulo.getEstado();
     	return model;
+    }
+    
+    @RequestMapping(value = "file-aporte.html", method = RequestMethod.POST)
+    public String handleFormUpload(@RequestParam("file") MultipartFile file) {
+
+        if (!file.isEmpty()) {
+            try {
+				byte[] bytes = file.getBytes();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+           return "redirect:uploadSuccess";
+       } else {
+           return "redirect:uploadFailure";
+       }
     }
 }
